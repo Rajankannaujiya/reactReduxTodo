10effc00d44225c46bec296c5c0f0d337283c4ee
anthusiast coder <139620395+Rajankannaujiya@users.noreply.github.com>
2024-04-13 11:19:27 +0530

    Update AddTodo.js

--- myapp/src/Components/AddTodo.js
+++ myapp/src/Components/AddTodo.js
@@ -7,6 +7,9 @@ import { addTodo } from '../Features/todo/todoSlice';
 function AddTodo() {
 
     const [input, setInput] = useState('')
+    // useDispatch React-Redux se aata h wireup ke liye reduce toolkit ke sath
+    // In the context of Redux, "dispatch" refers to the process of sending an action to the Redux store.
+    // dispatch ek reducer ko use karte huye store me change karta h
     const dispatch = useDispatch()
     const addTodoHandler = (e) => {
         e.preventDefault()
